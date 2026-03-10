f2945662e366c84d8a72294772afc50d5f402ae6
illumfx <hi@illumfx.de>
2026-03-10 14:13:45 +0100

    Added assets

--- frontend/src/pages/AchievementsPage.tsx
+++
@@ -1,7 +0,0 @@
-import ComingSoonPage from "./ComingSoonPage";
-
-function AchievementsPage() {
-  return <ComingSoonPage title="Achievements" />;
-}
-
-export default AchievementsPage;
--- frontend/src/pages/LeaderboardPage.tsx
+++
@@ -1,7 +0,0 @@
-import ComingSoonPage from "./ComingSoonPage";
-
-function LeaderboardPage() {
-  return <ComingSoonPage title="Leaderboard" />;
-}
-
-export default LeaderboardPage;
